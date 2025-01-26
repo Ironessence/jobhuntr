7912a5b80bc35097d4a3a349f3f83e758a0df836
Alexandru <alexdnc22@gmail.com>
2025-01-26 18:44:44 +0200

    30 to 40 on max duration for questions

--- src/app/api/(interview-prep)/generate-interview-questions/route.ts
+++ src/app/api/(interview-prep)/generate-interview-questions/route.ts
@@ -4,7 +4,7 @@ import { ObjectId } from "mongodb";
 import { NextRequest, NextResponse } from "next/server";
 import OpenAI from "openai";
 
-export const maxDuration = 30;
+export const maxDuration = 40;
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
