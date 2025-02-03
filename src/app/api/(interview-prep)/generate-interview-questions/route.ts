31ddc60a65993381c614c67d2072eaa8585178de
Alexandru <alexdnc22@gmail.com>
2025-02-03 17:18:45 +0200

    add extra timeout on generation

--- src/app/api/(interview-prep)/generate-interview-questions/route.ts
+++ src/app/api/(interview-prep)/generate-interview-questions/route.ts
@@ -4,7 +4,7 @@ import { ObjectId } from "mongodb";
 import { NextRequest, NextResponse } from "next/server";
 import OpenAI from "openai";
 
-export const maxDuration = 40;
+export const maxDuration = 60;
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
